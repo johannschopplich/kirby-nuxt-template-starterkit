57c5c158bf9cabf430105b5e6677c473423a13fe
Johann Schopplich <mail@johannschopplich.com>
2022-09-26 08:42:03 +0200

    fix: `onServerPrefetch` warnings

--- plugins/site.ts
+++ plugins/site.ts
@@ -1,12 +1,12 @@
 export default defineNuxtPlugin(async () => {
   const site = useSite()
 
-  if (Object.keys(site.value).length) return
-
-  try {
-    const { data } = await useKirbyData('_site')
-    site.value = data.value || {}
-  } catch (e) {
-    console.error('Error loading site data:', (e as Error).message)
+  if (!Object.keys(site.value).length) {
+    try {
+      const response = await $kirby<Record<string, any>>('_site')
+      site.value = response || {}
+    } catch (e) {
+      console.error('Error loading site data:', (e as Error).message)
+    }
   }
 })
