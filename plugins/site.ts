734509e57eb27067bdd953db96423ed411c63490
Johann Schopplich <mail@johannschopplich.com>
2022-12-22 12:46:14 +0100

    chore: upgrade dependencies

--- plugins/site.ts
+++ plugins/site.ts
@@ -1,12 +1,13 @@
+import type { FetchError } from 'ofetch'
+
 export default defineNuxtPlugin(async () => {
   const site = useSite()
 
-  if (!Object.keys(site.value).length) {
-    try {
-      const response = await $kirby<Record<string, any>>('_site')
-      site.value = response || {}
-    } catch (e) {
-      console.error('Error loading site data:', (e as Error).message)
-    }
+  try {
+    // Response will be cached in payload by default, thus no need to
+    // handle server/client side differently
+    site.value = await $kirby('_site')
+  } catch (e) {
+    console.error('Error loading site data:', (e as FetchError).message)
   }
 })
