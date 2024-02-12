512506bc1cf45249de58203be93e112a6aa375c1
Johann Schopplich <mail@johannschopplich.com>
2024-02-12 14:44:50 +0100

    chore: migrate to Kirby headless v3

--- plugins/site.ts
+++ plugins/site.ts
@@ -6,7 +6,7 @@ export default defineNuxtPlugin(async () => {
   try {
     // Response will be cached in payload by default, thus no need to
     // handle server/client side differently
-    site.value = await $kirby('_site')
+    site.value = await $kirby('__templates__/__site__')
   } catch (e) {
     console.error('Error loading site data:', (e as FetchError).message)
   }
